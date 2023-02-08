fb4bb69519d31502c1f13a091b83536f4c72a246
beaudry <beaudrychase@utexas.edu>
2023-02-08 15:52:58 -0600

    secrets

--- include/secrets.h
+++
@@ -1,9 +0,0 @@
-#define BOTtoken "1707512527:AAHNhiRD9hEDA0lwNwV2uosUyHVREX__MAE"
-#define CHAT_ID "1536236197"
-#define WIFI_SSID "p/g"
-#define WIFI_PASS "#0Ne23456789"
-#define OTA_PASS "beauchase213"
-
-#define TIMEZONE_API_KEY "5ANOLCRSRKFE"
-#define LATITUDE "30.267153"
-#define LONGITUDE "-97.743057"
