08481e170f2dd3936f3a5ea8971a9c387ba2b44e
Beaudry Chase <beauchase213@gmail.com>
2023-02-08 18:09:30 -0600

    Update structure (#12)
    * added template secret file

---
+++ include/template.secrets.h
@@ -0,0 +1,9 @@
+#define BOTtoken "<Telegram Bot Token>"
+#define CHAT_ID "<Telegram Chat Id>"
+#define WIFI_SSID "<ssid>"
+#define WIFI_PASS "<wifi password>"
+#define OTA_PASS "<OTA update password>"
+
+#define TIMEZONE_API_KEY "<timezonedb API Key>"
+#define LATITUDE "<geographic lat>"
+#define LONGITUDE "<geographic long>"
