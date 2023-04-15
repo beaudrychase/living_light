a891ad9a7164e2eba52c199f358b321f80ac25a8
Beaudry Chase <beauchase213@gmail.com>
2023-04-14 21:28:08 -0400

    testing new function

--- src/Classes/Breath.h
+++ src/Classes/Breath.h
@@ -21,9 +21,13 @@ private:
     const double _beta = 0.5; // shifts the gaussian to be symmetric
     SmoothColor oldColor = SmoothColor(0.0);
     DitherManager _ditherManager = DitherManager();
+    double const _blue = exp(-(_beta * _beta) / (2 * _colorGammaCorrection * _gam * _gam));
+    double const _red = -1.0 / (2 * _colorGammaCorrection * SMOOTHNESS * SMOOTHNESS * _gam * _gam);
+    double const _yellow = _beta / (_colorGammaCorrection * SMOOTHNESS * _gam * _gam);
     inline float computeBrightnessVal(int step, double total_steps)
     {
-        return (4 * DITHER_LEVEL) + (251.0 * DITHER_LEVEL * pow((exp(-(pow(((((double)step) / total_steps) - _beta) / _gam, 2.0)) / 2.0)), 1.0 / _colorGammaCorrection));
+            return (4 * DITHER_LEVEL) + (251.0 * DITHER_LEVEL * _blue * exp(_red * step * step + _yellow * step));
+            // return (4 * DITHER_LEVEL) + (251.0 * DITHER_LEVEL * pow((exp(-(pow(((((double)step) / total_steps) - _beta) / _gam, 2.0)) / 2.0)), 1.0 / _colorGammaCorrection));
     }
     void breathIteration(SmoothColor color, int arrayLen, float *brightnessArray, int offset);
 
